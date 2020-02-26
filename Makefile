748199a4668410e23443ce63979152d31b6d73f3
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-02-26 21:07:27 +0900

    Initial commit of python package structures

---
+++ Makefile
@@ -0,0 +1,23 @@
+package_name = jig-py
+
+export PATH := venv/bin:$(shell echo ${PATH})
+
+.PHONY: setup
+setup:
+	[ -d venv ] || python3 -m venv venv
+	pip3 install twine wheel pytest pip-tools
+	#pip3 install -r requirements.txt
+
+.PHONY: release
+release: clean build
+	python -m twine upload \
+		--repository-url https://upload.pypi.org/legacy/ \
+		dist/*
+
+.PHONY: build
+build: clean
+	python setup.py sdist bdist_wheel
+
+.PHONY: clean
+clean:
+	rm -rf $(subst -,_,${package_name}).egg-info dist build
---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+
+name = 'jig-py'
+version = '0.0.1'
+description = 'Jig for Python'
+dependencies = []
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+packages = [
+    package for package in setuptools.find_packages()
+    if package.startswith('jig')
+]
+
+namespaces = ['jig']
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author="Jig-Py Project Team",
+    author_email="dev+jig-py-contact@levii.co.jp",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/levii/jig-py",
+    packages=packages,
+    namespaces=namespaces,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=dependencies,
+)
