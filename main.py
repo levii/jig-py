a1284130d18309f6876c195cff46ac7b1db63df3
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-03-08 12:57:24 +0900

    最初のスケルトンを実装

---
+++ main.py
@@ -0,0 +1,63 @@
+import dataclasses
+import sys
+from typing import List
+
+
+# Domains
+
+@dataclasses.dataclass(frozen=True)
+class SourceCodeAST:
+    pass
+
+
+@dataclasses.dataclass(frozen=True)
+class ModulePath:
+    _path: str
+
+
+@dataclasses.dataclass(frozen=True)
+class ImportModule:
+    module_path: ModulePath
+
+
+@dataclasses.dataclass(frozen=True)
+class ImportModuleCollection:
+    _modules: List[ImportModule]
+
+
+@dataclasses.dataclass(frozen=True)
+class SourceFile:
+    path: str
+    size: int
+
+
+@dataclasses.dataclass(frozen=True)
+class SourceCode:
+    file: SourceFile
+    ast: SourceCodeAST
+    import_modules: ImportModuleCollection
+
+
+# Applications & Infrastructures
+
+class SourceCodeCollector:
+    def collect(self, target_path: str) -> SourceCode:
+        return SourceCode(
+            file=SourceFile(
+                path=target_path,
+                size=0,
+            ),
+            ast=SourceCodeAST(),
+            import_modules=ImportModuleCollection([])
+        )
+
+
+def collect(target_path: str) -> SourceCode:
+    collector = SourceCodeCollector()
+
+    return collector.collect(target_path=target_path)
+
+
+if __name__ == "__main__":
+    result = collect(target_path=sys.argv[1])
+    print(result)
