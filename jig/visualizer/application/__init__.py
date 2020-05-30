847199010c59292d58b72f288d90c8553a8f1eb2
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-05-30 15:37:57 +0900

    TextVisualizer を複数ソースコードを取り扱えるように修正

--- jig/visualizer/application/__init__.py
+++ jig/visualizer/application/__init__.py
@@ -4,15 +4,15 @@ from jig.collector.domain import SourceCode
 
 
 class DependencyTextVisualizer:
-    def __init__(self, source_code: SourceCode, module_names: List[str]):
-        self._source_code = source_code
+    def __init__(self, source_codes: List[SourceCode], module_names: List[str]):
+        self._source_codes = source_codes
         self._module_names = module_names
 
     def visualize(self) -> str:
         result = []
-        for dep in self._source_code.module_dependencies(
-            module_names=self._module_names
-        ):
-            result.append(f"{dep[0]} -> {dep[1]}")
+        for source_code in self._source_codes:
+            result.append(f"---- {source_code.file.module_path}")
+            for dep in source_code.module_dependencies(module_names=self._module_names):
+                result.append(f"{dep[0]} -> {dep[1]}")
 
         return "\n".join(result)
--- main.py
+++ main.py
@@ -9,16 +9,17 @@ def main():
     # とりあえずこのmain.pyファイルのパスをルートとする
     root_path = os.path.dirname(os.path.abspath(__file__))
 
-    source_codes = SourceCodeCollector(root_path=root_path).collect(target_path=sys.argv[1])
+    source_codes = SourceCodeCollector(root_path=root_path).collect(
+        target_path=sys.argv[1]
+    )
     print(source_codes)
 
     # print(.module_dependencies(["jig"]))
-    for source_code in source_codes:
-        print(
-            DependencyTextVisualizer(
-                source_code=source_code, module_names=["jig"]
-            ).visualize()
-        )
+    print(
+        DependencyTextVisualizer(
+            source_codes=source_codes, module_names=["jig"]
+        ).visualize()
+    )
 
 
 if __name__ == "__main__":
