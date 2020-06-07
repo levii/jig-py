e10c5ff23c84c2d034432ed85a98b21ffe572531
yosu <woodstock830@gmail.com>
2020-06-07 17:15:55 +0900

    モジュール依存性をdot形式で出力するVisualizerを追加

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 from jig.collector.application import SourceCodeCollector
-from jig.visualizer.application import DependencyTextVisualizer
+from jig.visualizer.application import DotTextVisualizer
 
 
 def main():
@@ -12,14 +12,21 @@ def main():
     source_codes = SourceCodeCollector(root_path=root_path).collect(
         target_path=sys.argv[1]
     )
-    print(source_codes)
+    # print(source_codes)
 
     # print(.module_dependencies(["jig"]))
-    print(
-        DependencyTextVisualizer(
-            source_codes=source_codes, module_names=["jig"]
-        ).visualize()
-    )
+    # print(
+    #     DependencyTextVisualizer(
+    #         source_codes=source_codes, module_names=["jig"]
+    #     ).visualize()
+    # )
+
+    dot = DotTextVisualizer(source_codes=source_codes, module_names=["jig"])
+
+    # 環境変数でlevelをもらう（CLI整備するまでの暫定）
+    level = int(os.environ.get("level", 3))
+
+    print(dot.visualize(level=level))
 
 
 if __name__ == "__main__":
