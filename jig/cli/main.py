c48c3b2c03bbc32ed63f901078ece5aaf794db5b
yosu <woodstock830@gmail.com>
2020-07-02 12:34:42 +0900

    コマンドのドキュメントを追加

--- jig/cli/main.py
+++ jig/cli/main.py
@@ -20,6 +20,13 @@ def _collect_source_codes(target_path: str) -> List[SourceCode]:
 
 
 def output_dependency_images(target_path, output_dir="output"):
+    """
+    指定されたディレクトリ以下を解析し、その結果を出力ディレクトリに出力します。
+    各Pythonファイルのモジュール名は、実行時のカレントディレクトリをPYTHONPATHとして解釈されます。
+    :param target_path: 解析対象のパスを指定します。
+    :param output_dir: 解析結果の出力ディレクトリを指定します（デフォルト: output）
+    :return:
+    """
     for depth in range(1, 6):
         source_codes = _collect_source_codes(target_path=target_path)
         dot = DependencyImageVisualizer(source_codes=source_codes, module_names=[])
