d9c8c8904145f0451f278dce9326fab45ffa98b1
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-07-12 14:35:52 +0900

    ImportDepentencyのテストケースを整理

--- tests/analyzer/domain/dependency/test_import_dependency.py
+++ tests/analyzer/domain/dependency/test_import_dependency.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from jig.analyzer.domain.dependency.import_dependency import ImportDependencyCollection
 from jig.collector.domain.source_code.source_code_import_dependency import (
     SourceCodeImportDependency,
@@ -6,16 +8,21 @@ from jig.collector.domain.values.import_path import ImportPath
 from jig.collector.domain.values.module_path import ModulePath
 
 
+def build_dep(
+    src_module_path: str, import_paths: List[str]
+) -> SourceCodeImportDependency:
+    return SourceCodeImportDependency.build(
+        source_module_path=ModulePath.from_str(src_module_path),
+        import_paths=[ImportPath.from_str(dest) for dest in import_paths],
+    )
+
+
 class TestImportDependencyCollection:
     def test_build(self):
         collection = ImportDependencyCollection.build(
             dependencies=[
-                SourceCodeImportDependency.build(
-                    source_module_path=ModulePath.from_str("main"),
-                    import_paths=[
-                        ImportPath.from_str("os.path"),
-                        ImportPath.from_str("typing.List"),
-                    ],
+                build_dep(
+                    src_module_path="main", import_paths=["os.path", "typing.List"]
                 )
             ]
         )
