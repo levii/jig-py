ce54912f2704233024d0ae737c9d48b94740520b
yosu <woodstock830@gmail.com>
2020-08-23 21:33:47 +0900

    クラスタ内でdigした時にちゃんとそのクラスタ内にノードが展開して表示されるように修正

--- jig/visualizer/module_dependency/domain/value/cluster.py
+++ jig/visualizer/module_dependency/domain/value/cluster.py
@@ -1,5 +1,5 @@
 import dataclasses
-from typing import Set, Dict
+from typing import Set, Dict, Optional
 
 from .module_node import ModuleNode
 
@@ -12,12 +12,29 @@ class Cluster:
 
     def to_dict(self) -> dict:
         nodes = sorted([n.name for n in self.children])
+        clusters = dict(
+            [(node.name, cluster.to_dict()) for node, cluster in self.clusters.items()]
+        )
 
-        return {"nodes": nodes}
+        return {"nodes": nodes, "clusters": clusters}
 
     @property
     def is_empty(self) -> bool:
-        return not bool(self.children)
+        if self.children:
+            return False
+
+        return all([cluster.is_empty for cluster in self.clusters.values()])
+
+    def find_node_owner(self, node: ModuleNode) -> Optional["Cluster"]:
+        if node in self.children:
+            return self
+
+        for cluster in self.clusters.values():
+            owner = cluster.find_node_owner(node)
+            if owner:
+                return owner
+
+        return None
 
     def add(self, node: ModuleNode):
         self.children.add(node)
@@ -28,6 +45,11 @@ class Cluster:
     def remove(self, node: ModuleNode):
         if node in self.children:
             self.children.remove(node)
+
+        for cluster in list(self.clusters.values()):
+            cluster.remove(node)
+            if cluster.is_empty:
+                del self.clusters[cluster.node]
 
     def hide_node(self, node: ModuleNode):
         if node in self.children:
--- tests/visualizer/module_dependency/domain/value/test_cluster.py
+++ tests/visualizer/module_dependency/domain/value/test_cluster.py
@@ -11,6 +11,27 @@ class TestCluster:
         c = Cluster(node=node("foo"))
         assert c.is_empty is True
 
+        child_cluster = Cluster(node=node("bar"))
+        c.add_cluster(child_cluster)
+
+        assert c.is_empty is True
+
+        child_cluster.add(node("buzz"))
+        assert c.is_empty is False
+
+    def test_find_node_owner(self):
+        c = Cluster(node=node("jig"), children={node("jig.cli")})
+
+        child_cluster = Cluster(
+            node=node("jig.collector"), children={node("jig.collector.application")}
+        )
+        c.add_cluster(child_cluster)
+
+        assert c.find_node_owner(node("foo")) is None
+        assert c.find_node_owner(node("jig")) is None
+        assert c.find_node_owner(node("jig.cli")) == c
+        assert c.find_node_owner(node("jig.collector.application")) == child_cluster
+
     def test_add(self):
         c = Cluster(node=node("foo"))
         c.add(node=node("bar"))
@@ -23,3 +44,14 @@ class TestCluster:
 
         c.remove(node=node("baz"))
         assert c.is_empty is True
+
+    def test_remove_with_child_cluster(self):
+        c = Cluster(node=node("jig"), children={node("jig.cli")})
+
+        child_cluster = Cluster(
+            node=node("jig.collector"), children={node("jig.collector.application")}
+        )
+        c.add_cluster(child_cluster)
+
+        c.remove(node=node("jig.collector.application"))
+        assert len(c.clusters) == 0
