d9f7ac728556c9ec7a8339d031d322773df50c58
yosu <woodstock830@gmail.com>
2020-09-14 16:39:01 +0900

    スタイル関連のテストを追加

--- tests/visualizer/module_dependency/presentation/renderer/test_graph_renderer.py
+++ tests/visualizer/module_dependency/presentation/renderer/test_graph_renderer.py
@@ -1,6 +1,7 @@
 from graphviz import Digraph
 
 from jig.visualizer.module_dependency.domain.model.graph import Graph
+from jig.visualizer.module_dependency.domain.model.master_graph import MasterGraph
 from jig.visualizer.module_dependency.domain.value.cluster import Cluster
 from jig.visualizer.module_dependency.domain.value.module_edge import (
     ModuleEdge,
@@ -11,6 +12,7 @@ from jig.visualizer.module_dependency.domain.value.module_node import (
     ModuleNodeStyle,
 )
 from jig.visualizer.module_dependency.domain.value.module_path import ModulePath
+from jig.visualizer.module_dependency.domain.value.penwidth import Color, PenWidth
 from jig.visualizer.module_dependency.presentation.renderer.graph_renderer import (
     GraphRenderer,
 )
@@ -63,5 +65,84 @@ class TestGraphRenderer:
 
         child.subgraph(grandchild)
         g.subgraph(child)
+
+        assert str(renderer.render()) == str(g)
+
+    def test_render__with_style(self):
+        master_graph = MasterGraph.from_tuple_list([("a", "b"), ("b", "c")])
+        graph = Graph(master_graph)
+
+        graph.style(
+            node("b"), color=Color.Blue, fontcolor=Color.Red, penwidth=PenWidth.Bold
+        )
+        renderer = GraphRenderer(graph=graph)
+
+        g = Digraph()
+        default_node_style = ModuleNodeStyle().to_dict()
+        node_style = ModuleNodeStyle().to_dict()
+        node_style.update(
+            {
+                "color": "blue",
+                "fontcolor": "red",
+                "penwidth": PenWidth.to_size(PenWidth.Bold),
+            }
+        )
+        g.node("a", **default_node_style)
+        g.node("b", **node_style)
+        g.node("c", **default_node_style)
+
+        edge_style = ModuleEdgeStyle().to_dict()
+        edge_style.update(
+            {
+                "color": "blue",
+                "fontcolor": "red",
+                "penwidth": PenWidth.to_size(PenWidth.Bold),
+            }
+        )
+        g.edge("a", "b", **edge_style)
+        g.edge("b", "c", **edge_style)
+
+        assert str(renderer.render()) == str(g)
+
+    def test_render__with_edge_style(self):
+        master_graph = MasterGraph.from_tuple_list([("a", "b"), ("b", "c")])
+        graph = Graph(master_graph)
+
+        graph.edge_style(node("a"), node("b"), color=Color.Red, penwidth=PenWidth.Thin)
+        renderer = GraphRenderer(graph=graph)
+
+        g = Digraph()
+        default_node_style = ModuleNodeStyle().to_dict()
+        g.node("a", **default_node_style)
+        g.node("b", **default_node_style)
+        g.node("c", **default_node_style)
+
+        default_edge_style = ModuleEdgeStyle().to_dict()
+        edge_style = ModuleEdgeStyle().to_dict()
+        edge_style.update({"color": "red", "penwidth": PenWidth.to_size(PenWidth.Thin)})
+        g.edge("a", "b", **edge_style)
+        g.edge("b", "c", **default_edge_style)
+
+        assert str(renderer.render()) == str(g)
+
+    def test_render__reset_style(self):
+        master_graph = MasterGraph.from_tuple_list([("a", "b"), ("b", "c")])
+        graph = Graph(master_graph)
+
+        graph.style(
+            node("b"), color=Color.Blue, fontcolor=Color.Red, penwidth=PenWidth.Bold
+        )
+        graph.reset_style()
+        renderer = GraphRenderer(graph=graph)
+
+        g = Digraph()
+        default_node_style = ModuleNodeStyle().to_dict()
+        g.node("a", **default_node_style)
+        g.node("b", **default_node_style)
+        g.node("c", **default_node_style)
+
+        default_edge_style = ModuleEdgeStyle().to_dict()
+        g.edge("a", "b", **default_edge_style)
+        g.edge("b", "c", **default_edge_style)
 
         assert str(renderer.render()) == str(g)
